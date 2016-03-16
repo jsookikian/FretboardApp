d778eb3c188fe7f91a0751f2c8f2b1a7071e7bb1
Jordan Sookikian <jsookikian@gmail.com>
2016-03-15 21:58:46 -0700

    Initial Commit

---
+++ FretboardApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FretboardApp
+//
+//  Created by Jordan Sookikian on 3/15/16.
+//  Copyright © 2016 Jordan Sookikian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
